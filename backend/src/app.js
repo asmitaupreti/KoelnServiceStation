cd1be84707bdca22e58379d26cbfb9a98e7afded
asmitaupreti <asmitaupreti52@gmail.com>
2024-06-17 12:17:07 +0200

    added helmet , express-mongo-sanitize and express-rate-limit for basic security

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import sanitize from 'express-mongo-sanitize'
+import { rateLimit } from 'express-rate-limit'
+import helmet from 'helmet'
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  message: 'Too many requests from this IP, please try again later',
+})
 
 const app = express()
 app.use(
@@ -11,6 +20,10 @@ app.use(
 
 app.use(express.json({ limit: '16kb' }))
 app.use(express.urlencoded({ extended: true, limit: '16kb' }))
+
+app.use(helmet())
+app.use(sanitize({ allowDots: true }))
+app.use(limiter)
 
 //import routes
 import stationRouter from './routes/station.routes.js'
