4a3b2e737238c220e5f3a6d9884e15069d89354c
asmitaupreti <asmitaupreti52@gmail.com>
2024-06-19 11:19:10 +0200

    Updated the sort function

--- frontend/src/hooks/useSortData.js
+++ frontend/src/hooks/useSortData.js
@@ -2,15 +2,20 @@ import { useMemo } from 'react'
 
 const useSortData = (data, sortOrder) => {
   const sortedData = useMemo(() => {
+    if (!sortOrder) {
+      return data
+    }
     return [...data].sort((a, b) => {
       if (sortOrder === 'asc') {
         return a.attributes.adresse.localeCompare(b.attributes.adresse, 'de', {
           numeric: true,
         })
-      } else {
+      } else if (sortOrder === 'desc') {
         return b.attributes.adresse.localeCompare(a.attributes.adresse, 'de', {
           numeric: true,
         })
+      } else {
+        return 0 // No change if sortOrder is invalid
       }
     })
   }, [data, sortOrder])
